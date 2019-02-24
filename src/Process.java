53c0c871dcb9a0972aeb61047aac1c90a291fa24
fadi-saad <fadoxin@gmail.com>
2019-02-24 16:55:26 +0200

    Added 2 types of processes

--- src/Process.java
+++ src/Process.java
@@ -1,4 +1,30 @@
 
-public class Process {
+public abstract class Process {
+	private int id;
+	private int priority;
+	private int memory;       //size of memory process owns
+	private String state;
+	private int memoryStart;  //first index in the array representing memory
 
+	public Process(int id, int priority,int memory,String state,int memoryStart) {
+		this.id = id;
+		this.priority = priority;
+		this.memory=memory;
+		this.state=state;
+		this.memoryStart=memoryStart;
+	}
+
+	public int getID() {
+		return id;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	public int getMemory() {
+		return memory;
+	}
+	
+	
 }
