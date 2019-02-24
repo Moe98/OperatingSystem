cab0f916a965d285e0cb97ff79ea6d8718a9194f
Moe98 <moeadele98@gmail.com>
2019-02-24 15:35:17 +0200

    Added User and Kernel

--- src/Kernel.java
+++ src/Kernel.java
@@ -1,4 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Kernel {
+	private int id;
+	private Queue<Process> processQueue;
 
+	public Kernel(int id) {
+		this.id = id;
+		processQueue = new LinkedList<>();
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public Queue<Process> getQueue() {
+		return processQueue;
+	}
+
+	public void pushProcess(Process process) {
+		try {
+			getQueue().add(process);
+		} catch (Exception e) {
+			
+		}
+	}
 }
